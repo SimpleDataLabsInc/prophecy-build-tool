8957c7b78c37da50fe5c9ba813417c3c7839c7cd
Kiran Karanth <kirankaranth1@gmail.com>
2022-09-27 07:20:22 +0530

    Windows Support

--- src/pbt/process.py
+++ src/pbt/process.py
@@ -10,17 +10,21 @@ class Process:
         std_output=subprocess.PIPE,
         std_err=subprocess.PIPE,
         is_shell=False,
+        running_message=""
     ):
         self.process_args = process_args
         self.current_working_directory = current_working_directory
         self.std_output = std_output
         self.std_err = std_err
         self.is_shell = is_shell
+        self.running_message = running_message
 
     @staticmethod
     def process_sequential(processes, time_between_each_cmd=1):
         return_code = 0
         for process in processes:
+            if process.running_message:
+                print(process.running_message)
             result = subprocess.run(
                 process.process_args,
                 stdout=process.std_output,
@@ -34,26 +38,27 @@ class Process:
                 result.stderr,
             )
 
-            if len(stderr) > 0:
-                print(
-                    "   ",
-                    "\n    ".join(
-                        [line.decode("utf-8") for line in stdout.splitlines()]
-                    ),
-                )
-                print(
-                    "   ",
-                    "\n    ".join(
-                        [line.decode("utf-8") for line in stderr.splitlines()]
-                    ),
-                )
-            else:
-                print(
-                    "   ",
-                    "\n    ".join(
-                        [line.decode("utf-8") for line in stdout.splitlines()]
-                    ),
-                )
+            if stdout is not None:
+                if stderr is not None and len(stderr) > 0:
+                    print(
+                        "   ",
+                        "\n    ".join(
+                            [line.decode("utf-8") for line in stdout.splitlines()]
+                        ),
+                    )
+                    print(
+                        "   ",
+                        "\n    ".join(
+                            [line.decode("utf-8") for line in stderr.splitlines()]
+                        ),
+                    )
+                else:
+                    print(
+                        "   ",
+                        "\n    ".join(
+                            [line.decode("utf-8") for line in stdout.splitlines()]
+                        ),
+                    )
 
             if return_code != 0:
                 break
