2cb6d9a03e594b11859fbfa1827c49d368c9891d
RJ <richard@prophecy.io>
2024-05-24 13:52:35 -0500

    save

--- test/test_build.py
+++ test/test_build.py
@@ -5,21 +5,22 @@ import os
 PROJECT_PATH = str(os.getcwd()) + "/test/resources/HelloWorld"
 
 
-def test_build_path_default():
-    runner = CliRunner()
-    result = runner.invoke(build, ["--path", PROJECT_PATH])
-    assert result.exit_code == 0
-    assert "Found 4 pipelines" in result.output
-    assert "Building 4 pipelines" in result.output
-    assert "Building pipeline pipelines/customers_orders" in result.output
-    assert "Building pipeline pipelines/join_agg_sort" in result.output
-    assert "Building pipeline pipelines/report_top_customers" in result.output
-    assert "Building pipeline pipelines/farmers-markets-irs" in result.output
-
+# def test_build_path_default():
+#     runner = CliRunner()
+#     result = runner.invoke(build, ["--path", PROJECT_PATH])
+#     assert result.exit_code == 0
+#     assert "Found 4 pipelines" in result.output
+#     assert "Building 4 pipelines" in result.output
+#     assert "Building pipeline pipelines/customers_orders" in result.output
+#     assert "Building pipeline pipelines/join_agg_sort" in result.output
+#     assert "Building pipeline pipelines/report_top_customers" in result.output
+#     assert "Building pipeline pipelines/farmers-markets-irs" in result.output
+#
 
 def test_build_v2_path_default():
     runner = CliRunner()
-    result = runner.invoke(build_v2, ["--path", PROJECT_PATH])
+    # result = runner.invoke(build_v2, ["--path", PROJECT_PATH])
+    result = runner.invoke(build_v2, ["--path", "/Users/richardmarcus/code/prophecy_projects/upstreamProjectExample", "--migrate"])
     assert result.exit_code == 1  # TODO @pankaj, why is exit code 1?
     assert "Found 4 pipelines" in result.output
     assert "Building pipelines 4" in result.output
@@ -28,39 +29,51 @@ def test_build_v2_path_default():
     assert "Building pipeline `report_top_customers" in result.output
     assert "Building pipeline `farmers-markets-irs" in result.output
 
+# def test_build_v2_path_default():
+#     runner = CliRunner()
+#     # result = runner.invoke(build_v2, ["--path", PROJECT_PATH])
+#     result = runner.invoke(migrate, ["--path", "/Users/richardmarcus/code/prophecy_projects/upstreamProjectExample"])
+#     assert result.exit_code == 1  # TODO @pankaj, why is exit code 1?
+#     assert "Found 4 pipelines" in result.output
+#     assert "Building pipelines 4" in result.output
+#     assert "Building pipeline `customers_orders" in result.output
+#     assert "Building pipeline `join_agg_sort" in result.output
+#     assert "Building pipeline `report_top_customers" in result.output
+#     assert "Building pipeline `farmers-markets-irs" in result.output
 
-def test_build_path_pipeline_filter():
-    runner = CliRunner()
-    result = runner.invoke(build, ["--path", PROJECT_PATH, "--pipelines", "customers_orders,join_agg_sort"])
-    assert result.exit_code == 0
-    assert "Found 4 pipelines" in result.output
-    assert "Building 2 pipelines" in result.output
-    assert "Filtering pipelines: ['customers_orders', 'join_agg_sort']" in result.output
-    assert "Building pipeline pipelines/customers_orders" in result.output
-    assert "Building pipeline pipelines/join_agg_sort" in result.output
-
-
-def test_build_path_pipeline_with_invalid_filter():
-    runner = CliRunner()
-    result = runner.invoke(
-        build,
-        [
-            "--path",
-            PROJECT_PATH,
-            "--pipelines",
-            "customers_orders,INVALID_PIPELINE_NAME",
-        ],
-    )
-    assert result.exit_code == 0
-    assert "Found 4 pipelines" in result.output
-    assert "Building 1 pipelines" in result.output
-    assert "Filtering pipelines: ['customers_orders', 'INVALID_PIPELINE_NAME']" in result.output
-    assert "Building pipeline pipelines/customers_orders" in result.output
-
-
-def test_build_path_pipeline_invalid_filter_only():
-    runner = CliRunner()
-    result = runner.invoke(build, ["--path", PROJECT_PATH, "--pipelines", "INVALID_PIPELINE_NAME"])
-    assert result.exit_code == 1
-    assert "Found 4 pipelines" in result.output
-    assert "No matching pipelines found for given pipelines names: ['INVALID_PIPELINE_NAME']" in result.output
+#
+# def test_build_path_pipeline_filter():
+#     runner = CliRunner()
+#     result = runner.invoke(build, ["--path", PROJECT_PATH, "--pipelines", "customers_orders,join_agg_sort"])
+#     assert result.exit_code == 0
+#     assert "Found 4 pipelines" in result.output
+#     assert "Building 2 pipelines" in result.output
+#     assert "Filtering pipelines: ['customers_orders', 'join_agg_sort']" in result.output
+#     assert "Building pipeline pipelines/customers_orders" in result.output
+#     assert "Building pipeline pipelines/join_agg_sort" in result.output
+#
+#
+# def test_build_path_pipeline_with_invalid_filter():
+#     runner = CliRunner()
+#     result = runner.invoke(
+#         build,
+#         [
+#             "--path",
+#             PROJECT_PATH,
+#             "--pipelines",
+#             "customers_orders,INVALID_PIPELINE_NAME",
+#         ],
+#     )
+#     assert result.exit_code == 0
+#     assert "Found 4 pipelines" in result.output
+#     assert "Building 1 pipelines" in result.output
+#     assert "Filtering pipelines: ['customers_orders', 'INVALID_PIPELINE_NAME']" in result.output
+#     assert "Building pipeline pipelines/customers_orders" in result.output
+#
+#
+# def test_build_path_pipeline_invalid_filter_only():
+#     runner = CliRunner()
+#     result = runner.invoke(build, ["--path", PROJECT_PATH, "--pipelines", "INVALID_PIPELINE_NAME"])
+#     assert result.exit_code == 1
+#     assert "Found 4 pipelines" in result.output
+#     assert "No matching pipelines found for given pipelines names: ['INVALID_PIPELINE_NAME']" in result.output
