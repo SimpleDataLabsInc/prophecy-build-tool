631afc25cbba26b29b37237ca930f12e5e53d36e
RJ <richard@prophecy.io>
2024-08-12 17:15:40 -0500

    small cleanup

--- test/test_tagging.py
+++ test/test_tagging.py
@@ -5,6 +5,7 @@ import os
 import shutil
 import uuid
 from git import Repo
+from parameterized import parameterized
 
 CURRENT_DIRECTORY = os.path.dirname(os.path.abspath(__file__))
 REPO_PATH = os.path.dirname(CURRENT_DIRECTORY)
@@ -16,14 +17,14 @@ class TaggingTestCase(unittest.TestCase):
 
     @staticmethod
     def _get_tmp_sample_repo(repo_url=SAMPLE_REPO):
-        new_path = os.path.join("/tmp/", SAMPLE_REPO.split("/")[-1], f"-{uuid.uuid4()}")
+        new_path = os.path.join("/tmp/", SAMPLE_REPO.split("/")[-1], f"{uuid.uuid4()}")
         repo = Repo.clone_from(repo_url, new_path)
         return repo, new_path
 
     def setUp(self):
         self.repo, self.repo_path = TaggingTestCase._get_tmp_sample_repo()
         self.python_project_path = os.path.join(self.repo_path, "prophecy")
-        self.python_project_path = os.path.join(self.repo_path, "prophecy_scala")
+        self.scala_project_path = os.path.join(self.repo_path, "prophecy_scala")
 
     def tearDown(self):
         if self.repo_path:
@@ -46,8 +47,9 @@ class TaggingTestCase(unittest.TestCase):
         assert result.exit_code == 0
         assert custom_tag in self.repo.tags
 
-    def test_tagging_default(self):
-        project_path = self.python_project_path
+    @parameterized.expand(["python", "scala"])
+    def test_tagging_default(self, language):
+        project_path = self.python_project_path if language == 'python' else self.scala_project_path
         pbt_version = TaggingTestCase._get_pbt_version(project_path)
 
         branch_name = "custom_branch_unittest"
@@ -59,8 +61,6 @@ class TaggingTestCase(unittest.TestCase):
         result = runner.invoke(tag, ["--path", project_path, "--repo-path", self.repo_path,
                                      "--no-push"])
         assert result.exit_code == 0
-
-        self.repo.tags
         assert custom_tag in [str(t) for t in self.repo.tags]
 
     def test_tagging_omit_branchname(self):
