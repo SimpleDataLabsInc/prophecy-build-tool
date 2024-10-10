cbb13c9a775f1b5e96564b601dee50e9c68f04ad
Sumit Paul <sumitpaul2508@gmail.com>
2024-10-10 12:22:50 +0530

    added html report generation

--- src/pbt/prophecy_build_tool.py
+++ src/pbt/prophecy_build_tool.py
@@ -732,7 +732,7 @@ class ProphecyBuildTool:
                         "--cov=.",  # generate coverage for module test
                         "--cov-report=xml",  # XML format
                         "--junitxml=report.xml",
-                        "--html=report.html --self-contained-html",
+                        "--html=report.html",
                         f"test{os.sep}TestSuite.py",
                     ],
                     path_pipeline_absolute,
