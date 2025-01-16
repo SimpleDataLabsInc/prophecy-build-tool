ce0b880fbe9e306566db557cc0e3c29059a3185c
Vaibhav Gakhreja <vgakhreja@Vaibhavs-MacBook-Pro.local>
2025-01-17 00:44:25 +0530

    .

--- src/pbt/utils/project_config.py
+++ src/pbt/utils/project_config.py
@@ -237,6 +237,10 @@ class JobInfo(BaseModel):
     def pause(self, flag: bool):
         self.is_paused = flag
 
+    def __post_init_post_parse__(self):
+        if self.type == SchedulerType.Databricks:
+            self.is_paused = not self.is_paused
+
     @staticmethod
     def create_job(
         name: str,
@@ -372,6 +376,7 @@ class JobsState(BaseModel):
         # then we create
 
         for job_response in filtered_response:
+            job_response.job_info.toggle_is_paused
             job_info = job_response.job_info
 
             if job_response.operation == OperationType.DELETED:
