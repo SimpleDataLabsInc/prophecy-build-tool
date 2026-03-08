99a246ee75825b69963abc9c3424427896eda659
rashmin <patel.rashmin@prophecy.io>
2026-03-08 16:54:06 +0530

    always build both Scala 2.12 and 2.13 JARs during release (#187)

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as _in:
 
 setuptools.setup(
     name="prophecy-build-tool",
-    version="1.3.16",
+    version="1.3.17",
     author="Prophecy",
     author_email="maciej@prophecy.io",
     description="Prophecy-build-tool (PBT) provides utilities to build and distribute projects created from the "
